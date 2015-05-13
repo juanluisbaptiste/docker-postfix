cb373828fc9210a13265eddef0b986912ac15d7b
Juan Luis Baptiste <juan.baptiste@gmail.com>
2015-05-13 01:06:38 -0500

    Set myhostname and myorigin, some providers like 1&1 will do MX lookups on them so they need to be valid.

--- run.sh
+++ run.sh
@@ -4,11 +4,17 @@
 [ -z "${SMTP_USERNAME}" ] && echo "SMTP_USERNAME is not set" && exit 1
 [ -z "${SMTP_PASSWORD}" ] && echo "SMTP_PASSWORD is not set" && exit 1
 
+DOMAIN=`echo $SERVER_HOSTNAME |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
+
+echo "myhostname=$SERVER_HOSTNAME"  >> /etc/postfix/main.cf
+echo "mydomain=$DOMAIN"  >> /etc/postfix/main.cf
+echo 'myorigin=$mydomain'  >> /etc/postfix/main.cf
 echo "relayhost = [$SMTP_SERVER]:587" >> /etc/postfix/main.cf
 echo "smtp_use_tls=yes" >> /etc/postfix/main.cf
 echo "smtp_sasl_auth_enable = yes" >> /etc/postfix/main.cf
 echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" >> /etc/postfix/main.cf
 echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
+
 echo "[$SMTP_SERVER]:587 $SMTP_USERNAME:$SMTP_PASSWORD" >> /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
 
