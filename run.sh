4a528463dac7a07d22044dd0c7083c8426694471
Juan Luis Baptiste <juan.baptiste@gmail.com>
2015-05-22 23:45:58 -0500

    Also set mydestination.

--- run.sh
+++ run.sh
@@ -4,10 +4,15 @@
 [ -z "${SMTP_USERNAME}" ] && echo "SMTP_USERNAME is not set" && exit 1
 [ -z "${SMTP_PASSWORD}" ] && echo "SMTP_PASSWORD is not set" && exit 1
 
+#Get the domain from the server host name
 DOMAIN=`echo $SERVER_HOSTNAME |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
+
+#Comment default mydestination, we will set it bellow
+sed -i -e '/mydestination/ s/^#*/#/' /etc/postfix/main.cf
 
 echo "myhostname=$SERVER_HOSTNAME"  >> /etc/postfix/main.cf
 echo "mydomain=$DOMAIN"  >> /etc/postfix/main.cf
+echo 'mydestination=$myhostname'  >> /etc/postfix/main.cf
 echo 'myorigin=$mydomain'  >> /etc/postfix/main.cf
 echo "relayhost = [$SMTP_SERVER]:587" >> /etc/postfix/main.cf
 echo "smtp_use_tls=yes" >> /etc/postfix/main.cf
