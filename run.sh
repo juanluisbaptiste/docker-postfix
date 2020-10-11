654becbfb5c5d344fa157112694bff8332f32839
David Cooper <david@dtcooper.com>
2020-10-10 20:07:20 -0700

    Fix issue #35

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ DOMAIN=`echo ${SERVER_HOSTNAME} |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^
 # Set needed config options
 add_config_value "myhostname" ${SERVER_HOSTNAME}
 add_config_value "mydomain" ${DOMAIN}
-add_config_value "mydestination" '$myhostname'
+add_config_value "mydestination" 'localhost'
 add_config_value "myorigin" '$mydomain'
 add_config_value "relayhost" "[${SMTP_SERVER}]:${SMTP_PORT}"
 add_config_value "smtp_use_tls" "yes"
